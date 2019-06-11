6190d2a96dafb68d542c5c1f72b681def507a600
karichalobato <00002517@uca.edu.sv>
2019-06-11 12:42:11 -0600

    Vaccine DAO

---
+++ app/src/main/java/com/grupo04/huellitas/database/daos/VaccineDAO.kt
@@ -0,0 +1,24 @@
+package com.grupo04.huellitas.database.daos
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.grupo04.huellitas.database.entities.Vaccine
+
+@Dao
+interface VaccineDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVaccine(vaccine: Vaccine)
+
+    @Query("SELECT * FROM Vaccine WHERE name==:name")
+    fun getVaccineByName(name: String): LiveData<List<Vaccine>>
+
+    @Query("DELETE FROM Vaccine WHERE idVaccine==:idVaccine")
+    fun deleteVaccineByID(idVaccine: Int):LiveData<List<Vaccine>>
+
+    @Query("DELETE FROM Vaccine")
+    fun deleteVaccines()
+}
