9dadab9fbc5939226509d976483319a324b56b24
karichalobato <00002517@uca.edu.sv>
2019-06-15 14:14:27 -0600

    Conexión Firebase

---
+++ app/src/main/java/com/grupo04/huellitas/activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.grupo04.huellitas.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grupo04.huellitas.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
